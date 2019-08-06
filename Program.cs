d9c275aea3ab1bc70bb03c492dd98b742b36f975
Tammi Lee <tammilee219@gmail.com>
2019-08-06 12:23:56 -0700

    created Human class

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace human
+{
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health; 
+        public int Health
+        {
+            get { return health; }
+        }
+        public Human(string NameInput)
+        {
+            Name = NameInput;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+
+        public Human(string NameInput, int StrenthInput, int IntelligenceInput, int DexterityInput, int HealthInput)
+        {
+            Name = NameInput;
+            Strength = StrenthInput;
+            Intelligence = IntelligenceInput;
+            Dexterity = DexterityInput;
+            health = HealthInput;
+        }
+
+        public int Attack (Human target)
+        {
+            int BooBoo = target.health;
+            BooBoo -= 5 * attacker.Strength;
+            return BooBoo;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
